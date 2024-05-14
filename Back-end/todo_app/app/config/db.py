50fed3418e48e899a4ab0afa8cb279bb05199f43
afzalfaizi <afzaalm993@gmail.com>
2024-05-14 11:16:03 +0500

    Full Stack Todo App

--- Back-end/todo_app/app/config/db.py
+++ Back-end/todo_app/app/config/db.py
@@ -1,7 +1,7 @@
 from sqlmodel import SQLModel, create_engine
 import os
 
-connection_string = os.geteng('DATABASE_URI')
+connection_string = os.getenv('DATABASE_URI')
 engine = create_engine(connection_string)
 
 def create_Tables():
