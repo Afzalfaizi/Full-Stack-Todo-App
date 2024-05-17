d885b4831c34ce483121ac80abc103595ae143e8
afzalfaizi <afzaalm993@gmail.com>
2024-05-17 12:29:06 +0500

    React Icons Adding in Task Component

--- Front-end/todo-app/components/Task.tsx
+++ Front-end/todo-app/components/Task.tsx
@@ -1,9 +1,16 @@
 import { Todo } from "@/type";
-
+import { CiSquareCheck } from "react-icons/ci";
+import { FiEdit } from "react-icons/fi";
+import { FiTrash2 } from "react-icons/fi";
 export default function Task({task}:{task:Todo}) {
   return (
     <tr>
-      <td>{task.email}</td>
+      <td>{task.name}</td>
+      <td>
+      <CiSquareCheck />
+      <FiEdit />
+      <FiTrash2 />
+      </td>
     </tr>
   )
 }
