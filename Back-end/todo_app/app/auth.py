9c2d075b0368db0fa48c992dca85e0e2c1560085
afzalfaizi <afzaalm993@gmail.com>
2024-07-09 23:31:54 +0500

    Register User Functionality

--- Back-end/todo_app/app/auth.py
+++ Back-end/todo_app/app/auth.py
@@ -3,3 +3,5 @@ from sqlmodel import Session
 from typing import Annotated
 from todo_app.db import get_session
 from fastapi import Depends
+
+pwd_context = CryptContext(schemes="bcrypt", deprecated = "auto")
