1fb43bbe914c79d4a172ee2b6f9359938e3b7c7f
afzalfaizi <afzaalm993@gmail.com>
2024-07-13 14:17:47 +0500

    create access token

--- Back-end/todo_app/app/main.py
+++ Back-end/todo_app/app/main.py
@@ -69,9 +69,8 @@ def start():
     
 # login
 @app.post("/token", response_model= Token)
-async def login(form_data:Annotated[OAuth2PasswordRequestForm, Depends()],
-                session:Annotated[Session, Depends(get_session)]):
-    user =  authenticate_user(form_data.username, form_data.password,session)
+async def login(form_data:Annotated[OAuth2PasswordRequestForm, Depends()], session:Annotated[Session, Depends(get_session)]):            
+    user = authenticate_user(session)
     if not user:
         raise HTTPException(status_code=401, detail="Incorrect username or password")
 
